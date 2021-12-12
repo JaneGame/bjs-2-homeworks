3253f7470a3ac8c74c85b884868c39d10f5f1894
JaneGame <fibi92@mail.ru>
2021-12-12 21:29:31 +0300

    update task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,22 @@
+"use strict";
+
+
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  let x;
+  let D = b ** 2 - 4 * a * c;
+  if (D < 0){
+	  arr = [];
+  }
+  else if (D == 0){
+	  x = - b /( 2 * a );
+	  arr = [x];
+  } else {
+	  x = (-b + Math.sqrt(D) )/(2*a);
+	  arr = [ x, -x];
+  }
+  
   return arr; // array
 }
 
