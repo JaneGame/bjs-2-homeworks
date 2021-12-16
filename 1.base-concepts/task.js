e2d0cbd9ee5c985373d33edea75322513f2e7e05
JaneGame <fibi92@mail.ru>
2021-12-16 16:11:43 +0300

    HomeWork2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,17 +4,19 @@
 
 function solveEquation(a, b, c) {
   let arr;
-  let x;
-  let D = b ** 2 - 4 * a * c;
-  if (D < 0){
+  let root1;
+  let root2;
+  let discriminant = b ** 2 - 4 * a * c;
+  if (discriminant < 0){
 	  arr = [];
   }
-  else if (D == 0){
-	  x = - b /( 2 * a );
-	  arr = [x];
+  else if (discriminant == 0){
+	  root1 = - b /( 2 * a );
+	  arr = [root1];
   } else {
-	  x = (-b + Math.sqrt(D) )/(2*a);
-	  arr = [ x, -x];
+	  root1 = (-b + Math.sqrt(discriminant) )/(2*a);
+	  root2 = (-b - Math.sqrt(discriminant) )/(2*a);
+	  arr = [ root1, root2];
   }
   
   return arr; // array
@@ -22,8 +24,11 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  let credit = amount - contribution;
+  let month = (date - Date.now())/1000/60/60/24/30;
+  month = month.toFixed(0);
+  let payment = credit * (percent/12/100 + (percent/12/100 / (((1 + percent/12/100)**month - 1))));
+  totalAmount = payment * month;
 
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+  return Number(totalAmount.toFixed(2));
 }
