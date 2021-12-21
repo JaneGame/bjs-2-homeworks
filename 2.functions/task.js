7a28a6d29d96691154d687c78364ec9f4eca6ca2
JaneGame <fibi92@mail.ru>
2021-12-21 18:14:55 +0300

    HomeWork2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,62 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
 
-  // Ваш код
+  for(let i = 0; i<arr.length; i++)
+   {if (min > arr[i]){
+      min = arr[i];}
+    if (max < arr[i]){
+	  max = arr[i];}
+	  
+    sum = sum + arr[i];}
+   
+	let avg = sum/arr.length;
+	avg = avg.toFixed(2);
 
-  return { min: min, max: max, avg: avg };
+  return { min: Number(min), max: Number(max), avg: Number(avg) };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
-
+  for (let i = 0; i<arr.length; i++){
+  sum = sum + arr[i];
+  }
   return sum;
 }
 
-function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
+function makeWork(arrOfArr, worker2) {
   
-  return max;
+  let sum = 0;
+  let max = worker2(arrOfArr[0]);
+for (let i = 0; i<arrOfArr.length; i++){
+	
+  let sum = worker2(arrOfArr[i]);
+  if(max<sum){
+	  max = sum;
+  }
+  
+}return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+	let minim = arr[0];
+	let maxim = arr[0];
+	
+  for (let i = 0; i<arr.length; i++){
+	 if(minim>arr[i]){
+		 minim = arr[i];
+	 } 
+	 if(maxim<arr[i]){
+		 maxim = arr[i];
+	 }
+  } 
+  
+  sum = Math.abs(maxim-minim);
+  return sum;
+  
 }
