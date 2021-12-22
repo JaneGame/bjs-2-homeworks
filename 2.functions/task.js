b5fb94886b04c870b3d9c0fc75294c649d366159
JaneGame <fibi92@mail.ru>
2021-12-22 20:38:38 +0300

    reformat code

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -4,59 +4,50 @@ function getArrayParams(arr) {
   let max = arr[0];
   let sum = 0;
 
-  for(let i = 0; i<arr.length; i++)
-   {if (min > arr[i]){
-      min = arr[i];}
-    if (max < arr[i]){
-	  max = arr[i];}
-	  
-    sum = sum + arr[i];}
-   
-	let avg = sum/arr.length;
-	avg = avg.toFixed(2);
+  for (let i = 0; i < arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+    if (max < arr[i]) {
+      max = arr[i];
+    }
 
-  return { min: Number(min), max: Number(max), avg: Number(avg) };
+    sum = sum + arr[i];
+  }
+
+  let avg = sum / arr.length;
+  avg = Number(avg.toFixed(2));
+
+  return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
   let sum = 0;
 
-  for (let i = 0; i<arr.length; i++){
-  sum = sum + arr[i];
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
   }
   return sum;
 }
 
-function makeWork(arrOfArr, worker2) {
-  
+function makeWork(arrOfArr, func) {
+
   let sum = 0;
-  let max = worker2(arrOfArr[0]);
-for (let i = 0; i<arrOfArr.length; i++){
-	
-  let sum = worker2(arrOfArr[i]);
-  if(max<sum){
-	  max = sum;
-  }
-  
-}return max;
+  let max = func(arrOfArr[0]);
+  for (let i = 0; i < arrOfArr.length; i++) {
+
+    let sum = func(arrOfArr[i]);
+    if (max < sum) {
+      max = sum;
+    }
+
+  } return max;
 }
 
 // Задание 3
 function worker2(arr) {
-	let minim = arr[0];
-	let maxim = arr[0];
-	
-  for (let i = 0; i<arr.length; i++){
-	 if(minim>arr[i]){
-		 minim = arr[i];
-	 } 
-	 if(maxim<arr[i]){
-		 maxim = arr[i];
-	 }
-  } 
-  
-  sum = Math.abs(maxim-minim);
-  return sum;
-  
+  let arrayParams = getArrayParams(arr);
+  return Math.abs(arrayParams.max - arrayParams.min);
+
 }
