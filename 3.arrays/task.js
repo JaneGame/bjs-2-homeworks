77f26b070819995ef33047362b0c1536478bf558
JaneGame <fibi92@mail.ru>
2022-01-02 22:11:40 +0300

    HomeWork3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,14 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result=arr1.every((item)=>arr1[item] === arr2[item]);
 
-  // Ваш код
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter(item => item%3 == 0).filter(item => item > 0).map(item => item * 10);
 
-  // Ваш код
+
 
   return resultArr; // array
 }
