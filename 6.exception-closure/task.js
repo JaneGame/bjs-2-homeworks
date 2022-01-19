a2e0319b15f95849d0b51abb777bd5277fc89f81
JaneGame <fibi92@mail.ru>
2022-01-19 19:19:24 +0300

    exception-closure

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,31 +1,34 @@
 //Задача 1
 
 function parseCount(number){
-    if (Number.parseInt(number, 10) == NaN) {
+    let parse = Number.parseInt(number, 10);
+    if (isNaN(parse)) {
         throw new Error("Невалидное значение");
     }
-    return Number.parseInt(number, 10);
+    return parse;
     
 }
 
 function validateCount(number){
     try{
         return parseCount(number);
-    }catch(err){
-        return 'Внимание! Произошла страшная ошибка!';
+    }catch(Error){
+        return Error;
     }
 }
 
 //Задача 2
 class Triangle{
     constructor(side1, side2, side3){
-    /*    if (side1<(side2+side3)&&side2<(side1+side3)&&side3<(side1+side2)) {
-        throw new Error("Треугольник с такими сторонами не существует");
-    }*/
+        if ((side1>(side2+side3))||(side2>(side1+side3))||(side3>(side1+side2))) {
+            this.error=new Error("Треугольник с такими сторонами не существует")
+        throw this.error;
+    } 
+        
         this.side1 = side1;
         this.side2 = side2;
         this.side3 = side3;
-        
+    
     }
 
     
@@ -36,7 +39,28 @@ class Triangle{
     }
 
     getArea(){
-        let area = Math.sqrt(this.getPerimeter()/2*(this.getPerimeter()/2-this.side1)*(this.getPerimeter()/2-this.side2)*(this.getPerimeter()/2-this.side3));
+        const semiPerimeter = this.getPerimeter()/2;
+        let area = Math.sqrt(semiPerimeter*(semiPerimeter-this.side1)*(semiPerimeter-this.side2)*(semiPerimeter-this.side3));
         return Number(area.toFixed(3)); 
     }
+}
+
+function getTriangle(side1, side2, side3) {
+
+    try{
+        let triangle = new Triangle(side1, side2, side3);
+        return triangle;
+    }catch(Error){
+        return {
+            getPerimeter() {              
+                return "Ошибка! Треугольник не существует"},
+
+            getArea() {   
+                return "Ошибка! Треугольник не существует"},
+        }
+    }
+
+    
+    
+
 }
