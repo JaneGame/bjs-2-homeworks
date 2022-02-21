19fca89065577b7a17a7d976681fc84ce5c460bb
JaneGame <fibi92@mail.ru>
2022-02-21 18:08:23 +0300

    No debounceDecoratorNew

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,44 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  const cashe = [];
+  function cashing(...arg){
+    let idx = cashe.findIndex((item)=>item == func(...arg));
+    if (idx == -1){
+      cashe.push(func(...arg))
+      console.log("Вычисляем: " + func(...arg));
+      return "Вычисляем: " + func(...arg); 
+    } else {
+
+    }
+    if (cashe.length>5){
+      cashe.shift()
+    }
+      console.log("Из кэша: " + cashe[idx]);
+      return "Из кэша: " + cashe[idx];
+  }
+  return cashing;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
+function debounceDecoratorNew(func, ms) {
+  let allImmediate
+  return function(...massage){
+    if (isNaN(ms)){
+      allImmediate = setTimeout(() => {
+        func(...massage);     
+      })  
+    }
+    allImmediate = setTimeout(() => {
+      func(...massage);
+      clearTimeout(allImmediate);     
+    }, ms)      
+}}
 
 function debounceDecorator2(func) {
-  // Ваш код
+  function wrapper(...massage) {
+    wrapper.history.push(massage);
+    return func.call(this, ...massage);    
+  }    
+  wrapper.history = []; 
+  return wrapper;
+
 }
