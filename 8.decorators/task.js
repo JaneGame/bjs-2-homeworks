92a159aeacd980da1d21972c3bb54a4c43041552
JaneGame <fibi92@mail.ru>
2022-02-23 23:02:31 +0300

    final version

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,44 +1,55 @@
+//Задача 1
 function cachingDecoratorNew(func) {
-  const cashe = [];
-  function cashing(...arg){
-    let idx = cashe.findIndex((item)=>item == func(...arg));
-    if (idx == -1){
-      cashe.push(func(...arg))
-      console.log("Вычисляем: " + func(...arg));
-      return "Вычисляем: " + func(...arg); 
-    } else {
-
+  const cache = [];
+  function cashing(...args){
+    let hash = args;
+    if (hash in cache) {            
+      console.log("Из кэша: "+ cache[hash]); 
+      return "Из кэша: " + cache[hash];       
+    } else {let result = func(...args);
+      if (cache.length>=5){
+        cache.shift()
+      }           
+       cache[hash] = result;            
+       console.log("Вычисляем: " + result); 
+       return "Вычисляем: " + result;       
+      }
     }
-    if (cashe.length>5){
-      cashe.shift()
-    }
-      console.log("Из кэша: " + cashe[idx]);
-      return "Из кэша: " + cashe[idx];
+    return cashing;
   }
-  return cashing;
-}
 
-
+//Задача 2
 function debounceDecoratorNew(func, ms) {
-  let allImmediate
-  return function(...massage){
-    if (isNaN(ms)){
-      allImmediate = setTimeout(() => {
-        func(...massage);     
-      })  
+  let allImmediate;
+  let flag = false;
+  return function wrapper(...args) {
+    if (!flag) {
+      func.apply(this, args);  
     }
+  flag = true;
+	clearTimeout(allImmediate);
     allImmediate = setTimeout(() => {
-      func(...massage);
-      clearTimeout(allImmediate);     
-    }, ms)      
-}}
-
-function debounceDecorator2(func) {
-  function wrapper(...massage) {
-    wrapper.history.push(massage);
-    return func.call(this, ...massage);    
-  }    
-  wrapper.history = []; 
-  return wrapper;
-
+	    flag = false;
+    }, ms);
+  };
 }
+
+//Задача 3
+function debounceDecorator2(func, ms) {
+  let allImmediate;
+  let flag = false;
+  wrapper.count = 0;
+  function wrapper(...args) {
+  wrapper.count ++;
+    if (!flag) {
+      func.apply(this, args);  
+    }
+  flag = true;
+	clearTimeout(allImmediate);
+    allImmediate = setTimeout(() => {
+	  flag = false;
+    }, ms);  
+  };
+  return wrapper;  
+}
+
